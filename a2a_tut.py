800bafb221c60578078541c7680540e87a16bf70
Yashvardhan Goel <yash2002goel@gmail.com>
2025-04-20 20:58:41 +0530

    feat: add initial a2a tutorial script with resources for learning about the technology

---
+++ a2a_tut.py
@@ -0,0 +1,9 @@
+### as of now there is no publicly available production level a2a service or client, but here are some of the resources to lear more about the technology
+
+
+# google's own github repo for a2a
+# https://github.com/google/a2a
+
+# youtube video on a2a
+# https://www.youtube.com/watch?v=voaKr_JHvF4
+
