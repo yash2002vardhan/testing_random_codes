a49f8409edeaaf8bf7a62022ef5e3c3830a6bee2
Yashvardhan Goel <yash2002goel@gmail.com>
2025-06-09 11:41:50 +0530

    feat: jax (google) tutorial

---
+++ jax_tut.py
@@ -0,0 +1,30 @@
+## JAX: stands for JIT(Just In Time) Compilation Automatic Differentiation XLA (Accelerated Linear Algebra)
+
+# JAX as numpy
+import jax
+import jax.numpy as jnp
+
+a = jnp.array(['1','2','3','4','5'])
+b = jnp.array(['6','7','8','9','10'])
+
+c = a + b
+print(c)
+
+
+
+
+
+
+
+
+
+
+
+
+# JIT Compilation
+# Automatic Differentiation
+# Automatic Vectorization
+# Randomness 
+
+
+
